55850f16ffcb248cb65d529549a6c06849d2d3eb
krung2 <jungbin4337@dgsw.hs.kr>
2021-05-21 22:15:00 +0900

    Add: start webhook

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { scheduleJob } from 'node-schedule';
 import { PORT } from './config/dotenv';
 import ORMLoad from './connect/typeORM';
+import { sendWebHook } from './libs/sendWebHook.lib';
 import { updateData } from './update/update';
 
 class App {
@@ -19,6 +20,7 @@ const app: express.Application = new App().application;
 const serverStart = async (): Promise<void> => {
 
   await ORMLoad();
+  await sendWebHook('서버가 켜졌습니다!!');
 
   const schedule = scheduleJob('0 * * * *', async () => {
 
